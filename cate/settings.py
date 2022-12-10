141cea6fac3272ccb6f452a79c910ff0a2a945d5
Laurent FAVOLE <laurentfavole03@gmail.com>
2022-12-10 16:26:02 +0100

    Setup static files directories

--- cate/settings.py
+++ cate/settings.py
@@ -144,7 +144,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
-# STATICFILES_DIRS = ["static/"]
+STATICFILES_DIRS = ["src/"]
 STATIC_ROOT = "static/"
 
 MEDIA_URL = "media/"
