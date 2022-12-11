5c1448049ece40004cea84bcf916ea65acdd8a93
Laurent FAVOLE <laurentfavole03@gmail.com>
2022-12-11 17:33:41 +0100

    Fix markdown template tag

--- cate/templatetags/markdown.py
+++ cate/templatetags/markdown.py
@@ -17,9 +17,7 @@ from markdown.inlinepatterns import LINK_RE, LinkInlineProcessor
 class Error(Exception):
     pass
 
-
 class InvalidMarkdown(Error):
-
     def __init__(self, error: str, value: Optional[str] = None) -> None:
         self.error = error
         self.value = value
@@ -30,33 +28,28 @@ class InvalidMarkdown(Error):
         return f'{self.error} "{self.value}"';
 
 
-def get_site_domain() -> str:
-    # TODO: Get your site domain here
-    return 'example.com'
-
-
-def clean_link(href: str, site_domain: str) -> str:
-    if href.startswith('mailto:'):
-        email_match = re.match('^(mailto:)?([^?]*)', href)
+def clean_link(href: str) -> str:
+    if href.startswith("mailto:"):
+        email_match = re.match("^(mailto:)?([^?]*)", href)
         if not email_match:
-            raise InvalidMarkdown('Invalid mailto link', value=href)
+            raise InvalidMarkdown("Invalid mailto link", value = href)
 
         email = email_match.group(2)
         if email:
             try:
                 EmailValidator()(email)
             except ValidationError:
-                raise InvalidMarkdown('Invalid email address', value=email)
+                raise InvalidMarkdown("Invalid email address", value = email)
 
         return href
 
     # Remove fragments or query params before trying to match the url name
-    href_parts = re.search(r'#|\?', href)
+    href_parts = re.search(r"#|\?", href)
     if href_parts:
         start_ix = href_parts.start()
         url_name, url_extra = href[:start_ix], href[start_ix:]
     else:
-        url_name, url_extra = href, ''
+        url_name, url_extra = href, ""
 
     try:
         url = reverse(url_name)
@@ -67,24 +60,24 @@ def clean_link(href: str, site_domain: str) -> str:
 
     parsed_url = urlparse(href)
 
-    if parsed_url.netloc == site_domain:
+    if parsed_url.netloc == "":
         try:
             resolver_match = resolve(parsed_url.path)
         except Resolver404:
             raise InvalidMarkdown(
                 "Should not use absolute links to the current site.\n"
                 "We couldn't find a match to this URL. Are you sure it exists?",
-                value=href,
+                value = href,
             )
         else:
             raise InvalidMarkdown(
-                'Should not use absolute links to the current site.\n'
-                'Try using the url name "{}".'.format(resolver_match.url_name),
-                value=href,
+                f"Should not use absolute links to the current site.\n"
+                f'Try using the url name "{resolver_match.url_name}".',
+                value = href,
             )
 
-    if parsed_url.scheme not in ('http', 'https'):
-        raise InvalidMarkdown('Must provide an absolute URL (be sure to include https:// or http://)', href)
+    if parsed_url.scheme not in ("http", "https"):
+        raise InvalidMarkdown("Must provide an absolute URL (be sure to include https:// or http://)", href)
 
     return href
 
@@ -92,14 +85,12 @@ def clean_link(href: str, site_domain: str) -> str:
 class DjangoLinkInlineProcessor(LinkInlineProcessor):
     def getLink(self, data, index):
         href, title, index, handled = super().getLink(data, index)
-        site_domain = get_site_domain()
-        href = clean_link(href, site_domain)
+        href = clean_link(href)
         return href, title, index, handled
-
 
 class DjangoUrlExtension(md.Extension):
     def extendMarkdown(self, md, *args, **kwrags):
-        md.inlinePatterns.register(DjangoLinkInlineProcessor(LINK_RE, md), 'link', 160)
+        md.inlinePatterns.register(DjangoLinkInlineProcessor(LINK_RE, md), "link", 160)
 
 register = template.Library()
 
