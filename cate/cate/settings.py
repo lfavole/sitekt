ed994e8425fb2c34935f2b84120e1b71b9d6685d
Laurent FAVOLE <laurentfavole03@gmail.com>
2022-12-24 12:14:01 +0100

    Use tabs in settings.py

--- cate/cate/settings.py
+++ cate/cate/settings.py
@@ -31,63 +31,63 @@ SECRET_KEY = settings.SECRET_KEY or "+jt!%+%erdp^y7h37v#68x31+u9ut6^8zryj@#zmu5p
 DEBUG = settings.DEBUG
 
 if not DEBUG:
-    CSRF_COOKIE_SECURE = True
-    SESSION_COOKIE_SECURE = True
-    CONN_MAX_AGE = 600
-    ALLOWED_HOSTS = [settings.HOST]
+	CSRF_COOKIE_SECURE = True
+	SESSION_COOKIE_SECURE = True
+	CONN_MAX_AGE = 600
+	ALLOWED_HOSTS = [settings.HOST]
 else:
-    ALLOWED_HOSTS = ["localhost", "127.0.0.1", "192.168.43.206"]
+	ALLOWED_HOSTS = ["localhost", "127.0.0.1", "192.168.43.206"]
 
 # Application definition
 
 INSTALLED_APPS = [
-    "django.contrib.admin",
-    "django.contrib.admindocs",
-    "django.contrib.auth",
-    "django.contrib.contenttypes",
-    "django.contrib.sessions",
-    "django.contrib.messages",
-    "django.contrib.staticfiles",
-    "adminsortable2",
-    "easy_thumbnails",
-    "filer",
-    "mptt",
-    "cate",
-    "uservisit",
-    "espacecate",
-    # "aumonerie",
-    "calendrier_avent_2022",
+	"django.contrib.admin",
+	"django.contrib.admindocs",
+	"django.contrib.auth",
+	"django.contrib.contenttypes",
+	"django.contrib.sessions",
+	"django.contrib.messages",
+	"django.contrib.staticfiles",
+	"adminsortable2",
+	"easy_thumbnails",
+	"filer",
+	"mptt",
+	"cate",
+	"uservisit",
+	"espacecate",
+	# "aumonerie",
+	"calendrier_avent_2022",
 ]
 
 MIDDLEWARE = [
-    "django.middleware.security.SecurityMiddleware",
-    "django.contrib.sessions.middleware.SessionMiddleware",
-    "django.middleware.common.CommonMiddleware",
-    "django.middleware.csrf.CsrfViewMiddleware",
-    "django.contrib.auth.middleware.AuthenticationMiddleware",
-    "django.contrib.messages.middleware.MessageMiddleware",
-    "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "uservisit.middleware.UserVisitMiddleware",
+	"django.middleware.security.SecurityMiddleware",
+	"django.contrib.sessions.middleware.SessionMiddleware",
+	"django.middleware.common.CommonMiddleware",
+	"django.middleware.csrf.CsrfViewMiddleware",
+	"django.contrib.auth.middleware.AuthenticationMiddleware",
+	"django.contrib.messages.middleware.MessageMiddleware",
+	"django.middleware.clickjacking.XFrameOptionsMiddleware",
+	"uservisit.middleware.UserVisitMiddleware",
 ]
 
 ROOT_URLCONF = "cate.urls"
 
 TEMPLATES = [
-    {
-        "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
-        "APP_DIRS": True,
-        "OPTIONS": {
-            "context_processors": [
-                "django.template.context_processors.debug",
-                "django.template.context_processors.request",
-                "django.contrib.auth.context_processors.auth",
-                "django.contrib.messages.context_processors.messages",
-                "cate.context_processors.app_name",
-                "espacecate.context_processors.navbar_processor",
-            ],
-        },
-    },
+	{
+		"BACKEND": "django.template.backends.django.DjangoTemplates",
+		"DIRS": [],
+		"APP_DIRS": True,
+		"OPTIONS": {
+			"context_processors": [
+				"django.template.context_processors.debug",
+				"django.template.context_processors.request",
+				"django.contrib.auth.context_processors.auth",
+				"django.contrib.messages.context_processors.messages",
+				"cate.context_processors.app_name",
+				"espacecate.context_processors.navbar_processor",
+			],
+		},
+	},
 ]
 
 WSGI_APPLICATION = "cate.wsgi.application"
@@ -97,21 +97,21 @@ WSGI_APPLICATION = "cate.wsgi.application"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+	"default": {
+		"ENGINE": "django.db.backends.sqlite3",
+		"NAME": BASE_DIR / "db.sqlite3",
+	}
 } if settings.USE_SQLITE else {
-    "default": {
-        "ENGINE": "django.db.backends.mysql",
-        "NAME": settings.DB_NAME,
-        "USER": settings.DB_USER,
-        "PASSWORD": settings.DB_PASSWORD,
-        "HOST": (USERNAME + ".mysql." + PYTHONANYWHERE_SITE.replace("pythonanywhere.com", "pythonanywhere-services.com")) if PYTHONANYWHERE else settings.DB_HOST,
-        "OPTIONS": {
-            "init_command": 'SET sql_mode="STRICT_TRANS_TABLES"',
-        },
-    }
+	"default": {
+		"ENGINE": "django.db.backends.mysql",
+		"NAME": settings.DB_NAME,
+		"USER": settings.DB_USER,
+		"PASSWORD": settings.DB_PASSWORD,
+		"HOST": (USERNAME + ".mysql." + PYTHONANYWHERE_SITE.replace("pythonanywhere.com", "pythonanywhere-services.com")) if PYTHONANYWHERE else settings.DB_HOST,
+		"OPTIONS": {
+			"init_command": "SET sql_mode=\"STRICT_TRANS_TABLES\"",
+		},
+	}
 }
 
 
@@ -119,18 +119,18 @@ DATABASES = {
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
-    {
-        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
-    },
-    {
-        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
-    },
-    {
-        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
-    },
-    {
-        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
-    },
+	{
+		"NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+	},
+	{
+		"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+	},
+	{
+		"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
+	},
+	{
+		"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+	},
 ]
 
 
