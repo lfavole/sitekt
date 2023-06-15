d7776bb172c443273305df8a2f1fa58e355359dc
lfavole <laurentfavole03@gmail.com>
2023-06-15 12:03:51 +0200

    Fix date input in subscription form

--- cate/espacecate/forms.py
+++ cate/espacecate/forms.py
@@ -1,8 +1,11 @@
 from django import forms
+from django.db import models
 
 from .models import Child
+
 
 class SubscriptionForm(forms.ModelForm):
 	class Meta:
 		model = Child
 		exclude = []
+		widgets = {models.DateField: {"widget": forms.widgets.DateInput}}
