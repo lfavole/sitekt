b6d804d551ce8dd3229e3ac79f018e24fe9b46d6
lfavole <laurentfavole03@gmail.com>
2023-07-23 16:04:41 +0200

    Fix navbar links

--- cate/cate/templatetags/nav.py
+++ cate/cate/templatetags/nav.py
@@ -1,7 +1,7 @@
 from typing import Any
 
 from django import template
-from django.urls import reverse
+from django.urls import NoReverseMatch, reverse
 from django.utils.html import escape
 from django.utils.safestring import mark_safe
 from espacecate.models import Page
@@ -13,11 +13,16 @@ def nav(value: list[tuple[Page, Any]]):
 	def nav_list(liste: list[tuple[Page, Any]]):
 		ret = "<ul>\n"
 		for page, nested_pages in liste:
+			href = None
 			if page.content == "":
 				href = "#"
-			elif page.content[0] != "#":
-				href = reverse(page.content)
-			else:
+			elif len(page.content) < 100 and page.content[0] != "<":
+				try:
+					href = reverse(page.content)
+				except NoReverseMatch:
+					href = None
+
+			if not href:
 				href = reverse("espacecate:page", args = [page.slug])
 
 			ret += "\t<li><a href=\"" + href + "\">"
