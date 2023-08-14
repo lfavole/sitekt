2645414017ecd31b0aebef480ce54b38daaea659
Laurent FAVOLE <laurentfavole03@gmail.com>
2023-08-14 12:08:59 +0000

    Fix slugify function

--- cate/cate/utils/text.py
+++ cate/cate/utils/text.py
@@ -6,7 +6,7 @@ def slugify(text):
     """
     Return a slugified version of the given text, suitable for file names or page URLs.
     """
-    text = unidecode(text)
+    text = unidecode(text.lower())
     text = text.replace("_", "-")
     text = re.sub(r"[^\w.]+", "-", text)
     text = re.sub(r"-{2,}", "-", text)
