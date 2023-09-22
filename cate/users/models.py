3fdb14f61887dfd0c41ffef00116d5efc6fc92d6
lfavole <laurentfavole03@gmail.com>
2023-09-22 19:01:08 +0200

    Add debug toolbar permission

--- cate/users/models.py
+++ cate/users/models.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
 
 
 class User(AbstractUser):
@@ -6,3 +7,9 @@ class User(AbstractUser):
 	User model for model swapping.
 	It's here if we might need to change the user model and don't unapply the migrations.
 	"""
+
+	class Meta:
+		verbose_name = _("user")
+		permissions = [
+			("can_see_debug_toolbar", "Can see debug toolbar"),
+		]
