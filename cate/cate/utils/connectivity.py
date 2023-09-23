db557416b509b35227f15e40130d8152a0b34714
lfavole <laurentfavole03@gmail.com>
2023-09-23 14:54:30 +0200

    Save connectivity result to speed up website

--- cate/cate/utils/connectivity.py
+++ cate/cate/utils/connectivity.py
@@ -1,4 +1,7 @@
 import socket
+from time import time
+
+result = (False, 0)
 
 
 def internet(host="8.8.8.8", port=53, timeout=3):
@@ -9,9 +12,16 @@ def internet(host="8.8.8.8", port=53, timeout=3):
     OpenPort: 53/tcp
     Service: domain (DNS/TCP)
     """
+    global result
+    if result[1] - time() < 30:
+        return result[0]
+
     try:
         socket.setdefaulttimeout(timeout)
         socket.socket(socket.AF_INET, socket.SOCK_STREAM).connect((host, port))
-        return True
+        ret = True
     except socket.error:
-        return False
+        ret = False
+
+    result = (time(), ret)
+    return ret
