70cf904adda5b1dac49dfc1de96624eab7cb0dea
Laurent FAVOLE <laurentfavole03@gmail.com>
2025-09-17 14:16:54 +0200

    feat: Update build script

--- build.sh
+++ build.sh
@@ -1,2 +1,18 @@
+echo "Installing gettext..."
+dnf install -y gettext
+
 python3 -m pip install --target . psycopg[binary,pool]~=3.2
-python3 manage.py setup
+
+echo "Collecting static files..."
+python3 manage.py collectstatic --noinput --clear -v 1 &
+
+echo "Creating the cache tables..."
+python3 manage.py createcachetable &
+
+echo "Migrating..."
+python3 manage.py migrate &
+
+echo "Compiling translations..."
+python3 manage.py compilemessages --ignore adminsortable2 --ignore allauth --ignore debug_toolbar --ignore django
+
+wait
