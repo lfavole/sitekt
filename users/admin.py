b3bfae44bf7ed728a686766ceea3d75197001952
Laurent FAVOLE <laurentfavole03@gmail.com>
2025-09-24 17:27:28 +0200

    feat: Add inline for email addresses

--- users/admin.py
+++ users/admin.py
@@ -1,6 +1,15 @@
+from allauth.account.models import EmailAddress
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
 
 from .models import User
 
-admin.site.register(User, UserAdmin)
+
+class EmailAddressInline(admin.TabularInline):
+    model = EmailAddress
+    extra = 0
+
+
+@admin.register(User)
+class UserAdmin(DjangoUserAdmin):
+    inlines = [EmailAddressInline]
