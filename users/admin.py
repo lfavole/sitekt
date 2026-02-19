11529be69769a2049333e7558538aed1dcd656fa
Laurent FAVOLE <laurentfavole03@gmail.com>
2026-02-19 16:21:30 +0100

    feat: Add webhooks

--- users/admin.py
+++ users/admin.py
@@ -1,8 +1,15 @@
+from html import escape
+
 from allauth.account.models import EmailAddress
+from allauth.core.internal import ratelimit
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
+from django.http import HttpResponseRedirect
+from django.http.request import HttpRequest
+from django.urls import path, reverse
+from django.utils.html import mark_safe
 
-from .models import User
+from .models import User, Webhook
 
 
 class EmailAddressInline(admin.TabularInline):
@@ -10,6 +17,42 @@ class EmailAddressInline(admin.TabularInline):
     extra = 0
 
 
+@admin.register(Webhook)
+class WebhookAdmin(admin.ModelAdmin):
+    raw_id_fields = ("user",)
+
+    def get_urls(self):
+        return [
+            path(
+                "<path:object_id>/send_test_payload/",
+                self.admin_site.admin_view(self.send_test_payload),
+                name=f"{self.opts.app_label}_{self.opts.model_name}_send_test_payload",
+            ),
+        ] + super().get_urls()
+
+    def send_test_payload(self, request, object_id):
+        # rate limit?
+        return Webhook.objects.get(pk=object_id).send_request({"child_name": "Test Test"})
+
+
+class WebhookInline(admin.TabularInline):
+    model = Webhook
+    extra = 0
+    fields = ["link"]
+    readonly_fields = ["link"]
+
+    def link(self, obj):
+        url = reverse(
+            "admin:%s_%s_change" % (self.opts.app_label, self.opts.model_name),
+            args=(obj.pk,),
+            current_app=self.admin_site.name,
+        )
+        return mark_safe(f'<a href="{escape(url)}" target="_blank">{escape(str(obj))}</a>')
+
+    def has_add_permission(self, *args, **kwargs):
+        return False
+
+
 @admin.register(User)
 class UserAdmin(DjangoUserAdmin):
-    inlines = [EmailAddressInline]
+    inlines = [EmailAddressInline, WebhookInline]
--- users/models.py
+++ users/models.py
@@ -1,4 +1,16 @@
+import json
+import re
+from html import escape
+from string import Template
+from urllib.parse import quote
+from urllib.request import Request, urlopen
+from wsgiref.util import is_hop_by_hop
+
+from django.conf import settings
 from django.contrib.auth.models import AbstractUser
+from django.core.serializers.json import DjangoJSONEncoder
+from django.db import models
+from django.http import HttpResponse
 from django.utils.translation import gettext_lazy as _
 
 
@@ -13,3 +25,102 @@ class User(AbstractUser):
         permissions = [
             ("can_see_debug_toolbar", "Can see debug toolbar"),
         ]
+
+
+class CustomTemplate(Template):
+    idpattern = r"\w(?:\.?\w)*(?::\w+)*"
+    flags = re.ASCII & re.IGNORECASE
+
+
+class CustomJSONEncoder(DjangoJSONEncoder):
+    def default(self, o):
+        if isinstance(o, (models.FileField, models.ImageField)):
+            return o.url if o and o.name else None
+        return super().default(o)
+
+
+def simplify(obj):
+    if isinstance(obj, (str, int, float, bool)):
+        return obj
+    if obj is None:
+        return None
+    if isinstance(obj, (list, tuple, set)):
+        return [simplify(item) for item in obj]
+    if isinstance(obj, dict):
+        return {simplify(key): simplify(value) for key, value in obj.items()}
+    try:
+        return CustomJSONEncoder().default(obj)
+    except TypeError:
+        return str(obj)
+
+
+class Webhook(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    url = models.URLField()
+    method = models.CharField(max_length=10, choices=[("GET", "GET"), ("POST", "POST"), ("PUT", "PUT"), ("DELETE", "DELETE")])
+    headers = models.TextField(blank=True)
+    body = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def send_request(self, data):
+        replace = self.make_replacer({"data": simplify(data)})
+        with urlopen(
+            Request(
+                url=replace(self.url),
+                data=replace(self.body).encode(),
+                headers=self.parse_headers(replace(self.headers)),
+                method=self.method,
+            )
+        ) as f:
+            headers = {key: value for key, value in dict(f.getheaders()).items() if not is_hop_by_hop(key)}
+            return HttpResponse(f.read(), status=f.getcode(), headers=headers)
+
+    def parse_headers(self, headers):
+        return {
+            name: value
+            for name, _, value
+            in (header.partition(": ") for header in headers.splitlines())
+        }
+
+    def make_replacer(self, data):
+        def replace(string):
+            template = CustomTemplate(string)
+            substitutions = {}
+
+            for identifier in template.get_identifiers():
+                key = identifier
+                key, sep, extra_params = key.partition(":")
+
+                value = data
+                for subkey in key.split("."):
+                    try:
+                        subkey = int(subkey)
+                    except ValueError:
+                        pass
+                    value = value[subkey]
+
+                extra_params = extra_params.split(":") if extra_params else None
+                for param in extra_params:
+                    substitutions[identifier] = self.escape(value, param)
+
+            return template.substitute(substitutions)
+
+        return replace
+
+    def escape(self, value, escape_type: str | None):
+        if escape_type == "json":
+            return json.dumps(value)
+
+        if escape_type == "url":
+            return quote(value)
+
+        if escape_type == "html":
+            return escape(value)
+
+        if escape_type == "" or escape_type is None:
+            return str(value)
+
+        raise ValueError(f"Invalid escape type: {escape_type}")
